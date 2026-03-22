05c9a743d0bb30b5f1eaccdac2d181953b7559c0
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2026-03-23 02:07:47 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'   // 引入中文语言包
+
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
 import App from './App.vue'
 
-createApp(App).use(ElementPlus).mount('#app')
+
+createApp(App).use(router).use(ElementPlus,{locale: zhCn,}).mount('#app')
