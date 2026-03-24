836f7c0540a150f681d447467c5e447ea0542152
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2026-03-25 00:03:15 +0800

    Create Admin.vue

---
+++ src/components/Admin.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <a href="./">返回主页</a>
+    </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      thisYear: new Date().getFullYear()
+    }},
+}
+</script>
+<style scoped>
+    .cr_text {
+        text-align: center;
+        color: #888;
+        margin: 20px 0;
+        font-size: 14px;
+    }
+</style>
