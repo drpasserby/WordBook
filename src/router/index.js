134fafa03f456cd9c3f438ef56e0ad5840198cd7
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2026-03-29 23:12:11 +0800

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../components/Home.vue'
+import Admin from '../components/Admin.vue'
+
+const routes = [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/admin', name: 'Admin', component: Admin },
+]
+
+const router = createRouter({
+  history: createWebHistory('/wordbook/'),
+  routes
+})
+
+
+export default router
